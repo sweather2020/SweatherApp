1c63e9eb924be6b7e95aced6b90954786c442571
wyy1123 <ianwu2022@u.northwestern.edu>
2020-02-25 20:05:34 -0600

    integrated weather and quote data from API

--- home.js
+++ home.js
@@ -1,4 +1,8 @@
 
+const initialLoading = () =>{
+    loadquote()
+    getWeather()
+}
 
 
 const loadquote = () =>{
@@ -10,4 +14,27 @@ const loadquote = () =>{
     document.getElementById('quote-content').innerHTML = `${data[Math.floor(Math.random()*data.length)].text} - ${data[Math.floor(Math.random()*data.length)].author}`
   });
 
+}
+
+function getWeather()
+{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Evanston&appid=aaa9323b5c8689c2a78bd9ba797828f0`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data.length == 0){
+                alert("No such location");
+            }
+            else{
+                document.getElementById('weather-content').innerHTML = '';
+                const template = `<section class="weather-card" id=${data.weather[0].id}>
+                                    <div>
+                                        <h3>Temperature is ${(data.main.temp).toFixed(0)} K</h3>
+                                        <h4>${String(data.weather[0].description)}</h4>
+                                    </div>
+                                </section>`;
+                document.getElementById('weather-content').innerHTML += template;
+            }
+            
+        });
 }
