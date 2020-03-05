5739b36b097093073f02fbf81cb50c7ad1413a3f
wyy1123 <ianwu2022@u.northwestern.edu>
2020-03-04 19:26:02 -0600

    displays diff names on homepage when login by diff users

--- login.php
+++ login.php
@@ -5,10 +5,15 @@ header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 $credentials = [
     'email' => 'brian@example.com',
-    'password' => '123456'
+    'password' => '123456',
+    'email2' => 'ian@example.com',
+    'password2'=> '123456'
+
+
 ];
 
-if ( $credentials['email'] !== $_POST['email'] OR $credentials['password'] !== $_POST['password'] )
+if (( $credentials['email'] !== $_POST['email'] OR $credentials['password'] !== $_POST['password'] ) 
+    AND ( $credentials['email2'] !== $_POST['email'] OR $credentials['password2'] !== $_POST['password'] ))
 {    
     header('Location: ' . $_SERVER['HTTP_REFERER']);
     exit();
@@ -19,6 +24,13 @@ session_start();
 // Storing session data
 $_SESSION["isLogged"] = "1";
 
-header('Location:' . '../home.php');
+if ( $credentials['email'] !== $_POST['email'] AND $credentials['password'] !== $_POST['password'] ) 
+{    
+    header('Location:' . '../home_brian.php');
+}
+if ( $credentials['email2'] !== $_POST['email'] AND $credentials['password2'] !== $_POST['password'] ) 
+{    
+    header('Location:' . '../home_ian.php');
+}
 
 exit();
