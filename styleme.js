ea7f50164695db91c64edb3af50d0f16a044c353
wobraddo <44324315+wobraddo@users.noreply.github.com>
2020-03-11 00:00:33 -0500

    styling update
    in line with page style

--- styleme.js
+++ styleme.js
@@ -6,21 +6,18 @@ function myFunction2() {
     document.getElementById("myDropdown2").classList.toggle("show");
   }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn1')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content1");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn1')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content1");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
       }
     }
-  }
-}
-
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn2')) {
+    else if (!event.target.matches('.dropbtn2')) {
       var dropdowns = document.getElementsByClassName("dropdown-content2");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
